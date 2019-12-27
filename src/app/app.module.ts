94ca1037bd32d3e1f928d39e108d88f0ed2fda04
Hem Sunder <hemsunder47@gmail.com>
2019-12-27 18:55:09 +0530

    offer calculation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FyleComponent } from './fyle/fyle.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { FyleComponent } from './fyle/fyle.component';
     FyleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/fyle/fyle.component.ts
+++ src/app/fyle/fyle.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-fyle',
-  templateUrl: './fyle.component.html',
-  styleUrls: ['./fyle.component.css']
+  selector: "app-fyle",
+  templateUrl: "./fyle.component.html",
+  styleUrls: ["./fyle.component.css"]
 })
-export class FyleComponent{
+export class FyleComponent {
+  constructor() {}
 
-  constructor() { }
+  numberServing: number;
+  offer: boolean = true;
+  lollipopPrice: number;
+  lollipopPriceOffer: number;
+  icecandyPrice: number;
+  icecandyPriceOffer: number;
+  priceDiff: number;
+  offer1: any;
 
-  changeCard25(){
-
+  changeCardVal(val) {
+    this.numberServing = val;
+    this.lollipopPrice = val;
+    this.lollipopPriceOffer = val / 2;
+    this.icecandyPrice = this.lollipopPrice * 2;
+    this.icecandyPriceOffer = this.icecandyPrice / 2;
+    this.priceDiff = this.icecandyPrice - this.lollipopPrice;
+    // this.offer1 = this.applyOffer();
   }
 
-
+  applyOffer() {
+    // this.numberServing = val;
+    this.offer = !this.offer;
+    // if (this.offer === false) {
+    //   this.lollipopPrice /= 2;
+    //   console.log(this.lollipopPrice);
+    //   this.icecandyPrice = (this.lollipopPrice * 2) / 2;
+    //   console.log(this.icecandyPrice);
+    // }
+  }
 }
